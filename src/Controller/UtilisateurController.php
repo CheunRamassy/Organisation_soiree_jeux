0a6e282a9a49f3c9190050fe9119bd54041bfa9a
CheunRamassy <cheunramassy.pr@gmail.com>
2025-01-17 10:51:45 +0400

    initialisation du crud UtilisateurController

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -7,12 +7,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UtilisateurController extends AbstractController
-{
-    #[Route('/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
-    {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
-    }
+{ 
+    // #[Route('/home', name: 'show_user', methods: ['GET'])]
+    // public function show(): Response
+    // {
+
+
+    // }
+
+    // #[Route('/create', name: 'new_user', methods: ['POST'])]
+    // public function new(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}/edit', name: 'edit_user', methods: ['PUT'])]
+    // public function edit(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}', name: 'delete_user', methods: ['POST'])]
+    // public function delete(): Response
+    // {
+
+
+    // }
 }
