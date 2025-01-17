f2a78e8662a69102f3ed0e738323259017e9c8ce
CheunRamassy <cheunramassy.pr@gmail.com>
2025-01-17 11:15:38 +0400

    Mise en place du show dans le JeuxController

--- src/Controller/JeuxController.php
+++ src/Controller/JeuxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\JeuxRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,26 +10,29 @@ use Symfony\Component\Routing\Attribute\Route;
 final class JeuxController extends AbstractController
 {
  
-    // #[Route('/home', name: 'show_event', methods: ['GET'])]
-    // public function show(): Response
-    // {
+    #[Route('/showJeux', name: 'show_jeux', methods: ['GET'])]
+    public function show(JeuxRepository $repository): Response
+    {
+        $Jeux = $repository->findAll();
 
+        return $this->render('jeux/index.html.twig', [
+            'Jeux' => $Jeux,
+        ]);
+    }
 
-    // }
-
-    // #[Route('/create', name: 'new_event', methods: ['POST'])]
+    // #[Route('/createJeux', name: 'new_jeux', methods: ['POST'])]
     // public function new(): Response
     // {
 
     // }
 
-    // #[Route('/{id}/edit', name: 'edit_event', methods: ['PUT'])]
+    // #[Route('/{id}/editJeux', name: 'edit_jeux', methods: ['PUT'])]
     // public function edit(): Response
     // {
 
     // }
 
-    // #[Route('/{id}', name: 'delete_event', methods: ['POST'])]
+    // #[Route('/{id}', name: 'delete_jeux', methods: ['POST'])]
     // public function delete(): Response
     // {
 
