ea7b25844e4e9f13a16c4f33a76bc2f070666894
CheunRamassy <cheunramassy.pr@gmail.com>
2025-01-17 09:29:54 +0400

    Création de l'entity Jeux

---
+++ src/Entity/Jeux.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuxRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuxRepository::class)]
+class Jeux
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $regle = null;
+
+    #[ORM\Column]
+    private ?int $nbPlayers = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRegle(): ?string
+    {
+        return $this->regle;
+    }
+
+    public function setRegle(string $regle): static
+    {
+        $this->regle = $regle;
+
+        return $this;
+    }
+
+    public function getNbPlayers(): ?int
+    {
+        return $this->nbPlayers;
+    }
+
+    public function setNbPlayers(int $nbPlayers): static
+    {
+        $this->nbPlayers = $nbPlayers;
+
+        return $this;
+    }
+}
