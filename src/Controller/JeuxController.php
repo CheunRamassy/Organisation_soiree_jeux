04a047577e51fa772b807f88e1a536acd52bea39
CheunRamassy <cheunramassy.pr@gmail.com>
2025-01-17 10:52:08 +0400

    initialisation du crud JeuxController

--- src/Controller/JeuxController.php
+++ src/Controller/JeuxController.php
@@ -8,11 +8,30 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class JeuxController extends AbstractController
 {
-    #[Route('/jeux', name: 'app_jeux')]
-    public function index(): Response
-    {
-        return $this->render('jeux/index.html.twig', [
-            'controller_name' => 'JeuxController',
-        ]);
-    }
+ 
+    // #[Route('/home', name: 'show_event', methods: ['GET'])]
+    // public function show(): Response
+    // {
+
+
+    // }
+
+    // #[Route('/create', name: 'new_event', methods: ['POST'])]
+    // public function new(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}/edit', name: 'edit_event', methods: ['PUT'])]
+    // public function edit(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}', name: 'delete_event', methods: ['POST'])]
+    // public function delete(): Response
+    // {
+
+
+    // }
 }
