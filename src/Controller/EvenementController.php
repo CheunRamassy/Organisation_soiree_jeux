2ed69ce6bb94499c0e093ce1a8bcefe846815d7c
CheunRamassy <cheunramassy.pr@gmail.com>
2025-01-17 10:50:50 +0400

    initialisation du crud EvenementController

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EvenementController extends AbstractController
 {
-    #[Route('/evenement', name: 'app_evenement')]
-    public function index(): Response
-    {
-        return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
-        ]);
-    }
+ 
+    // #[Route('/home', name: 'show_event', methods: ['GET'])]
+    // public function show(): Response
+    // {
+
+
+    // }
+
+    // #[Route('/create', name: 'new_event', methods: ['POST'])]
+    // public function new(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}/edit', name: 'edit_event', methods: ['PUT'])]
+    // public function edit(): Response
+    // {
+
+    // }
+
+    // #[Route('/{id}', name: 'delete_event', methods: ['POST'])]
+    // public function delete(): Response
+    // {
+
+
+    // }
+
 }
